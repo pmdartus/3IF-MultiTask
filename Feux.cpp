20c8ef5e05304843266cb1192d75ff93182ee6a2
nbuisson <nicolas.buisson@insa-lyon.fr>
2013-02-18 22:29:58 +0100

    Feux quasi-opérationnel, reste bug de temps N-S = 1 seconde

--- Feux.cpp
+++ Feux.cpp
@@ -31,6 +31,9 @@ using namespace std;
 static const int DUREE_ORANGE = 3;
 static const int DUREE_ROUGE = 2;
 static const int DUREE_ARRET = DUREE_ORANGE + DUREE_ROUGE;
+static const char VERT[] = "Vert  \0";
+static const char ORANGE[] = "Orange\0";
+static const char ROUGE[] = "Rouge \0";
 
 //------------------------------------------------------------------ Types
 
@@ -82,7 +85,7 @@ static void afficherDuree()
 {
 	Afficher(DUREE_AXE_NS, dureeNS, STANDARD, NORMALE);
 	Afficher(DUREE_AXE_EO, dureeEO, STANDARD, NORMALE);
-}
+} //----- fin de afficherDuree
 
 static void afficherNS(int duree, int dureeSup)
 // Mode d'emploi :
@@ -94,7 +97,7 @@ static void afficherNS(int duree, int dureeSup)
 {
 	Afficher(TEMPS_AXE_NS, duree, STANDARD, NORMALE);
 	Afficher(TEMPS_AXE_EO, duree + dureeSup, STANDARD, NORMALE);
-}
+} //----- fin de afficherNS
 
 static void afficherEO(int duree, int dureeSup)
 // Mode d'emploi :
@@ -106,7 +109,21 @@ static void afficherEO(int duree, int dureeSup)
 {
 	Afficher(TEMPS_AXE_EO, duree, STANDARD, NORMALE);
 	Afficher(TEMPS_AXE_NS, duree + dureeSup, STANDARD, NORMALE);
-}
+} //----- fin de afficherEO
+
+static void majDuree()
+// Mode d'emploi :
+//
+// Contrat :
+//
+// Algorithme :
+//
+{
+	semop(myIdDuree, &reserver, 1);
+	dureeEO = myMemDuree->eO;
+	dureeNS = myMemDuree->nS;
+	semop(myIdDuree, &liberer, 1);
+} //----- fin de majDuree
 
 static void initialisation()
 // Mode d'emploi :
@@ -121,23 +138,20 @@ static void initialisation()
 	myMemDuree = (Duree*) shmat(myIdDuree, NULL, 0);
 
 	// Initialisation des durées des feux
-	semop(myIdDuree, &reserver, 1);
-	dureeEO = myMemDuree->eO;
-	dureeNS = myMemDuree->nS;
-	semop(myIdDuree, &liberer, 1);
+	majDuree();
 
 	// Les feux de l'axe Nord-Sud se mettent au vert
 	myMemDuree->nS = true;
 
 	// Affichage des couleurs initiales
-	Afficher(COULEUR_AXE_NS, "Vert", GRAS, INVERSE);
-	Afficher(COULEUR_AXE_EO, "Rouge", GRAS, INVERSE);
+	Afficher(COULEUR_AXE_NS, VERT, GRAS, INVERSE);
+	Afficher(COULEUR_AXE_EO, ROUGE, GRAS, INVERSE);
 
 	// Affichage des durées initiales
 	Afficher(TEMPS_AXE_NS, dureeNS, STANDARD, NORMALE);
 	Afficher(TEMPS_AXE_EO, dureeNS + DUREE_ARRET, STANDARD, NORMALE);
 	afficherDuree();
-}
+} //----- fin de initialisation
 
 //////////////////////////////////////////////////////////////////  PUBLIC
 //---------------------------------------------------- Fonctions publiques
@@ -170,26 +184,63 @@ void ActiverFeux(int etatFeux, int duree, int sem)
 
 	for( ; ; )
 	{
-		Afficher(COULEUR_AXE_NS, "Vert", GRAS, INVERSE);
+		majDuree();
+		afficherDuree();
+
+		Afficher(COULEUR_AXE_NS, VERT, STANDARD, NORMALE);
 		for(int i = dureeNS ; i > 0 ; i--)
 		{
 			afficherNS(i, DUREE_ARRET);
 			sleep(1);
 		}
 
-		Afficher(COULEUR_AXE_NS, "Orange", GRAS, INVERSE);
+		Afficher(COULEUR_AXE_NS, ORANGE, STANDARD, NORMALE);
+		// Inversion de l'état de l'état du feu N-S
+		myMemEtatFeux->nS ^= true;
 		for(int i = 3 ; i > 0 ; i--)
 		{
 			afficherNS(i, DUREE_ROUGE);
 			sleep(1);
 		}
 
-		Afficher(COULEUR_AXE_NS, "Rouge", GRAS, INVERSE);
+		Afficher(COULEUR_AXE_NS, ROUGE, GRAS, INVERSE);
 		for(int i = 2 ; i > 0 ; i--)
 		{
 			afficherNS(i, 0);
 			sleep(1);
 		}
+
+		majDuree();
+		afficherDuree();
+
+		// Inversion de l'état du feux E-O
+		myMemEtatFeux->eO ^= true;
+
+		Afficher(COULEUR_AXE_EO, VERT, STANDARD, NORMALE);
+		for(int i = dureeEO ; i > 0 ; i--)
+		{
+			afficherEO(i, DUREE_ARRET);
+			sleep(1);
+		}
+
+		Afficher(COULEUR_AXE_EO, ORANGE, STANDARD, NORMALE);
+		// Inversion de l'état de l'état du feux E-O
+		myMemEtatFeux->eO ^= true;
+		for(int i = 3 ; i > 0 ; i--)
+		{
+			afficherEO(i, DUREE_ROUGE);
+			sleep(1);
+		}
+
+		Afficher(COULEUR_AXE_EO, ROUGE, GRAS, INVERSE);
+		for(int i = 2 ; i > 0 ; i--)
+		{
+			afficherEO(i, 0);
+			sleep(1);
+		}
+
+		// Inversion de l'état de l'état du feu N-S
+		myMemEtatFeux->nS ^= true;
 	}
 
 } //----- fin de ActiverFeux
